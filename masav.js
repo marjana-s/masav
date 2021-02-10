f2bffa7e7cfd437e53bc032dcfd75b19c76629f2
marjana-s <77576659+marjana-s@users.noreply.github.com>
2021-02-10 17:47:13 +0200

    index.html

---
+++ masav.js
@@ -0,0 +1,37 @@
+var game = []
+game.push('minecraft');
+game.push('pubg mobail');
+game.push('contr strike');
+game.push('among us');
+game.push('roblox');
+
+console.log(game.pop());
+console.log(game.pop());
+console.log(game.pop());
+console.log(game.pop());
+console.log(game.pop());
+
+
+
+var game = []
+game.push('minecraft');
+game.push('pubg mobail');
+game.push('contr strike');
+game.push('among us');
+game.push('roblox');
+
+console.log(game.shift());
+console.log(game.shift());
+console.log(game.shift());
+console.log(game.shift());
+console.log(game.shift());
+
+
+
+var arr = [8, 86, 2, 8, 4, 5, 70, 47];
+console.log(arr.toString());
+arr.sort();
+console.log(arr.toString());
+
+console.log("min " + Math.min(...arr));
+console.log("max " + Math.max(...arr));
